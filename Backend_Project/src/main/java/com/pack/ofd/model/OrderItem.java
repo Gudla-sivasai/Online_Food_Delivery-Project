a491dde6420035d18f812099a80a35cfae646a07
Gudla-sivasai <155511517+Gudla-sivasai@users.noreply.github.com>
2024-07-02 10:56:53 +0530

    Update OrderItem.java

--- Backend_Project/src/main/java/com/pack/ofd/model/OrderItem.java
+++ Backend_Project/src/main/java/com/pack/ofd/model/OrderItem.java
@@ -12,7 +12,9 @@ public class OrderItem {
 	private int itemId;
 	private int orderId;
 	private int foodId;
+    private int restaurantId;
 	private int quantity;
+	private String foodImgUrl;
 	
 	public int getItemId() {
 		return itemId;
@@ -31,12 +33,24 @@ public class OrderItem {
 	}
 	public void setFoodId(int foodId) {
 		this.foodId = foodId;
-	}
+	} 
+	public int getRestaurantId() {
+        return restaurantId;
+    }
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
 	public int getQuantity() {
 		return quantity;
 	}
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
+	}
+	public String getFoodImgUrl() {
+		return foodImgUrl;
+	}
+	public void setFoodImgUrl(String foodImgUrl) {
+		this.foodImgUrl = foodImgUrl;
 	}
 	
 }
