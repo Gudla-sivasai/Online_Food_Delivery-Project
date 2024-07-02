a158fc9ea939dd17eacf137c2a75625f93a0fcb3
Gudla-sivasai <155511517+Gudla-sivasai@users.noreply.github.com>
2024-07-02 11:00:58 +0530

    Update RestaurantRepository.java

--- Backend_Project/src/main/java/com/pack/ofd/repository/RestaurantRepository.java
+++ Backend_Project/src/main/java/com/pack/ofd/repository/RestaurantRepository.java
@@ -3,7 +3,9 @@ package com.pack.ofd.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pack.ofd.model.Restaurant;
+import com.pack.ofd.model.User;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer>{
 
+	public Restaurant findByEmailId(String emailId);
 }
