54ad4fd2b747c4690e195230fc1c920fc46f1168
Gudla-sivasai <155511517+Gudla-sivasai@users.noreply.github.com>
2024-07-02 11:06:45 +0530

    Update Body.js

--- Frontend_Project/src/components/Body/Body.js
+++ Frontend_Project/src/components/Body/Body.js
@@ -1,39 +1,17 @@
-import React from 'react';
-import RestaurantCards from './RestaurantCards';
-import Carousel from './Carousel';
-import CategorySlide from './CategoriesSlide';
+import React, { useEffect, useState } from "react";
+import { CategorySlide, Carousel, SearchBox } from "../Home/HomePage";
+import Footer from "../Footer/Footer";
 
-const SearchBox = () => {
-    return(
-        <>
-        <div className="container search-bar mt-5">
-            <form className="d-flex">
-                <input className="form-control me-2 px-5 shadow" list="datalistOptions" type="search" placeholder="Search"
-                    aria-label="Search"/>
-                <button className="btn btn-outline-primary shadow" type="submit">Search</button>
-                {/* <datalist id="datalistOptions">
-                    <option value="Biryani">
-                    <option value="Veg Biryani">
-                    <option value="Pot Biryani">
-                    <option value="Desert">
-                    <option value="Ice cream">
-                </datalist> */}
-            </form>
-        </div>
-        <hr className="border border-dark border-1 opacity-75 mx-5 mt-4"/>
-        </>
-    )
-}
-
+  
 const Body = () => {
-    return(
-        <>
-            <Carousel/>
-            <CategorySlide/>
-            <SearchBox/>
-            <RestaurantCards/>
-        </>
-    )
-}
+  return (
+    <>
+      <Carousel />
+      <CategorySlide />
+      <SearchBox />
+      <Footer/>
+    </>
+  );
+};
 
 export default Body;
